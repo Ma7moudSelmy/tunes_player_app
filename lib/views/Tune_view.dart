90f72779b8a4b270ea0cde686c06e211d4277c25
Ma7moudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-06-07 21:25:04 +0300

    Refactor Tuneview to use a dynamic list for TuneViewitems and improve code readability

--- lib/views/Tune_view.dart
+++ lib/views/Tune_view.dart
@@ -1,32 +1,35 @@
-import 'package:tunes_player_app/widgets/Tune_view%20items.dart';
 import 'package:flutter/material.dart';
+import 'package:tunes_player_app/widgets/Tune_view%20items.dart';
 
 class Tuneview extends StatelessWidget {
-  const Tuneview({super.key});
+  Tuneview({super.key});
+
+  final List<Color> itemscolors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+    Colors.pink,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Tune View\n Ma7moudSelmy"),
+        title: const Text("Flutter Tune View\n Ma7moudSelmy"),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 67, 68, 68),
         elevation: 0,
       ),
       body: Column(
-        children: const [
-          TuneViewitems(color: Colors.red),
-          TuneViewitems(color: Colors.orange),
-          TuneViewitems(color: Colors.yellow),
-          TuneViewitems(color: Colors.green),
-          TuneViewitems(color: Colors.blue),
-          TuneViewitems(color: Colors.indigo),
-          TuneViewitems(color: Colors.purple),
-          TuneViewitems(color: Colors.pink),
-          
-        
-        ],
+        children: itemscolors
+            .map((color) => TuneViewitems(color: color))
+            .toList(),
       ),
     );
   }
 }
+
